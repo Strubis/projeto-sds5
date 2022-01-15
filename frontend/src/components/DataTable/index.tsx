739322469c44558b7e9d6115063bc9da13053178
Emerson Lima <emersonlima910@gmail.com>
2022-01-15 12:51:19 -0200

    Componentes estaticos simples adicionados

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+const DataTable = () => {
+    return (
+        <div className="table-responsive">
+            <table className="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Data</th>
+                        <th>Vendedor</th>
+                        <th>Clientes visitados</th>
+                        <th>Negócios fechados</th>
+                        <th>Valor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>22/05/2019</td>
+                        <td>Barry</td>
+                        <td>34</td>
+                        <td>29</td>
+                        <td>15017.20</td>
+                    </tr>
+                    <tr>
+                        <td>22/05/2019</td>
+                        <td>Barry</td>
+                        <td>34</td>
+                        <td>29</td>
+                        <td>15017.20</td>
+                    </tr>
+                    <tr>
+                        <td>22/05/2019</td>
+                        <td>Barry</td>
+                        <td>34</td>
+                        <td>29</td>
+                        <td>15017.20</td>
+                    </tr>
+                    <tr>
+                        <td>22/05/2019</td>
+                        <td>Barry</td>
+                        <td>34</td>
+                        <td>29</td>
+                        <td>15017.20</td>
+                    </tr>
+                    <tr>
+                        <td>22/05/2019</td>
+                        <td>Barry</td>
+                        <td>34</td>
+                        <td>29</td>
+                        <td>15017.20</td>
+                    </tr>
+                    <tr>
+                        <td>22/05/2019</td>
+                        <td>Barry</td>
+                        <td>34</td>
+                        <td>29</td>
+                        <td>15017.20</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default DataTable;
